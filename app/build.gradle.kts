6c2c0b72dde7bdf3abc2a80a54a73699b40b5c71
Adam Levenstein <adam.levenstein@gmail.com>
2026-06-14 17:12:52 -0500

    Bumped version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lionstone.monkeyrapids"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 4
+        versionName = "0.1.3"
     }
 
     buildTypes {
